1cbe40da2a2a1052ae9d076d51dab153abbe4f2a
Nathan Walker <walkerrunpdx@gmail.com>
2016-04-14 09:04:17 -0700

    update

--- src/nativescript/node_modules/ngrx-store-router/middleware.d.ts
+++
@@ -1,4 +0,0 @@
-import 'rxjs/add/operator/do';
-import { Provider } from 'angular2/core';
-declare var _default: Provider;
-export default _default;
--- src/nativescript/platforms/android/src/main/assets/app/tns_modules/ngrx-store-router/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Provider } from 'angular2/core';
-declare const routerMiddleware: (Provider[] | Provider)[];
-export { RouterSubscriber } from './subscriber';
-export { routerReducer, RouterState, RouterActions } from './reducer';
-export { routerMiddleware };
